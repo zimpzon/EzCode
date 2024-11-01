6b3c9a23a4ec7abc134f8c36c6e4296eba857354
Peter Wennerstrøm Eriksen <peterwennerstrom.eriksen@ufst.dk>
2024-11-01 13:08:16 +0100

    backup

--- EzCode/Problems.cs
+++ EzCode/Problems.cs
@@ -1,17 +1,35 @@
-﻿namespace EzCode
+﻿using System.Reflection.Metadata;
+
+namespace EzCode
 {
-    // list of test [input + output]
+    public class InputParameter
+    {
+        public required Type Type { get; init; }
+        public required string Name { get; init; }
+    }
+
+    public class Function
+    {
+        public required string Name { get; init; }
+        public required List<Parameter> Parameters { get; init; }
+        public required Type ReturnType { get; init; }
+    }
+
+    public class VerificationTest
+    {
+        public required List<object> InputParameters { get; init; }
+        public required object ExpectedOutput { get; init; }
+    }
+
     public class Problem
     {
-        public Type ReturnType { get; init; }
-        public string FunctionName { get; init; }
-        public string Description { get; init; }
+        public required Function Function { get; init; }
+        public required string Description { get; init; }
+        public required List<VerificationTest> VerificationTests { get; init; }
     }
 
     internal static class Problems
     {
-        public static readonly List<Problem> List = [
-            new Problem { }
-        ];
+        public static readonly List<Problem> List = [];
     }
 }
