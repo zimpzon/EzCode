43aab7fd7d462e6efafa5b5b8c9e14021af7434c
Peter Wennerstrøm Eriksen <peterwennerstrom.eriksen@ufst.dk>
2024-11-08 12:30:09 +0100

    backup

---
+++ EzCodeProblems/Problem0002.cs
@@ -0,0 +1,16 @@
+﻿namespace EzCodeProblems
+{
+    // ideal: problems can be downloaded as a text file.
+    //   funtion is just declared with comments. We can find it either with attribute or just only func in the code?
+    //   verify... dsl?
+    public class Problem0002
+    {
+        // Return the sum of a and b.
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+
+    }
+}
